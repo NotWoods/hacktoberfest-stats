95ef6e2ea3056f0aa0cfa2c4e0c993e84f5c8cb5
Tiger Oakes <toakes@mozilla.com>
2019-11-01 20:41:10 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# hacktoberfest-stats
+
+Spits out some stats on the pull requests submitted to the
+[mozilla-mobile](https://github.com/mozilla-mobile/) organization during
+Hacktoberfest!
+
+## Setup
+
+```shell
+npm install
+echo "<Github personal access token>" > token
+npm start
+```
---
+++ script.js
@@ -0,0 +1,65 @@
+const { readFileSync, writeFile } = require('fs');
+const { promisify } = require('util');
+const Octokit = require('@octokit/rest');
+
+const octokit = Octokit({
+    auth: readFileSync('token', 'utf8').trim(),
+    baseUrl: 'https://api.github.com'
+});
+
+const writeFileAsync = promisify(writeFile);
+
+/**
+ * Returns usernames of all mozilla-mobile members.
+ * @returns {Promise<Set<string>>}
+ */
+async function listMembers() {
+    const options = octokit.orgs.listMembers.endpoint.merge({
+        org: 'mozilla-mobile'
+    });
+    const members = await octokit.paginate(options);
+    return new Set(members.map(member => member.login));
+}
+
+/**
+ * Returns async iterator over PR list in October.
+ * @param {number} year
+ * @param {Set<string>} ignoreUsers
+ */
+async function* hacktoberfestPrs(year, ignoreUsers) {
+    const options = octokit.search.issuesAndPullRequests.endpoint.merge({
+        q: [
+            'is:pr',
+            'org:mozilla-mobile',
+            `created:${year}-10-01..${year}-10-31`,
+            '-label:invalid',
+            '-author:MickeyMoz',
+            '-author:mozilla-l10n-automation-bot'
+        ].join(' ')
+    });
+    for await (const response of octokit.paginate.iterator(options)) {
+        for (const pr of response.data) {
+            if (!ignoreUsers.has(pr.user.login)) {
+                yield pr;
+            }
+        }
+    }
+}
+
+async function main(year) {
+    const members = await listMembers();
+    const allContributions = {};
+    for await (const pr of hacktoberfestPrs(year, members)) {
+        const contributions = allContributions[pr.user.login] || [];
+        contributions.push({
+            url: pr.html_url,
+            title: pr.title,
+            repo: pr.repository_url.substring('https://api.github.com/repos/'.length),
+        });
+        allContributions[pr.user.login] = contributions;
+    }
+
+    await writeFileAsync('contributions.json', JSON.stringify(allContributions));
+}
+
+main(2019);
